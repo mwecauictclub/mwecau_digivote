b1d837b37f18ef29a4ce92b0959309bc84d410ae
cleven12 <clevengodsontech@gmail.com>
2025-08-29 12:58:14 -0400

    db model and api update

--- src/backend/core/serializers.py
+++ src/backend/core/serializers.py
@@ -35,37 +35,3 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
             'refresh': str(refresh),
             'access': str(refresh.access_token),
         }
-
-#  Prev v[1]
-# from rest_framework import serializers
-# from election.models import User, State, Course
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ['registration_number', 'first_name', 'last_name', 'email', 'is_verified', 'role','course']
-
-# class ForgotPasswordSerializer(serializers.Serializer):
-#     registration_number = serializers.CharField(max_length=20)
-#     email = serializers.EmailField()
-#     state = serializers.PrimaryKeyRelatedField(queryset=State.objects.all())
-#     course = serializers.PrimaryKeyRelatedField(queryset=Course.objects.all())
-#     first_name = serializers.CharField(max_length=50, required=False)
-#     last_name = serializers.CharField(max_length=50, required=False)
-
-
-
-
-# Prev v[0]
-# from rest_framework import serializers
-# from election.models import User, State, Course
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ['registration_number', 'first_name', 'last_name', 'email', 'is_verified', 'role']
-# class ForgotPasswordSerializer(serializers.Serializer):
-#     registration_number = serializers.CharField(max_length=20)
-#     email = serializers.EmailField()
-#     state = serializers.PrimaryKeyRelatedField(queryset=State.objects.all())
-#     course = serializers.PrimaryKeyRelatedField(queryset=Course.objects.all())
-#     first_name = serializers.CharField(max_length=50, required=False)
-#     last_name = serializers.CharField(max_length=50, required=False)
