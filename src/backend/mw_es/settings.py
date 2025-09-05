b83f55a9d9d26bc30b7e7f044e2ce2d4893bf297
cleven12 <clevengodsontech@gmail.com>
2025-09-05 09:02:35 +0300

    mode: static files and design docs

--- src/backend/mw_es/settings.py
+++ src/backend/mw_es/settings.py
@@ -88,7 +88,7 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static/staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
